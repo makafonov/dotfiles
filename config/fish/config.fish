bf8b52e32ddc01e9e973e3a5bbd068b70c293daf
Denis S. <makafonov@gmail.com>
2023-09-10 13:06:29 +0500

    Add some prefs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,12 @@
+eval "$(/opt/homebrew/bin/brew shellenv fish)"
+
 source ~/.config/fish/alias.fish
+source "$(brew --prefix asdf)"/libexec/asdf.fish
 
-source /usr/local/opt/asdf/libexec/asdf.fish
-
-fish_add_path /usr/local/sbin
-# fish_add_path /usr/local/opt/openssl@1.1/bin
-fish_add_path /usr/local/opt/openssl@3/bin
+fish_add_path $(brew --prefix openssl@1.1)/bin
 fish_add_path $HOME/.local/bin
-fish_add_path /usr/local/opt/bzip2/bin
+
+set -U fish_greeting
 
 set -gx EDITOR nvim
 set -gx LC_ALL en_US.UTF-8
@@ -15,17 +15,16 @@ set -gx LANG en_US.UTF-8
 # disabling bytecode (.pyc) files
 set -gx PYTHONDONTWRITEBYTECODE 1
 
-# For compilers to find libffi
-# set -gx LDFLAGS "-L/usr/local/opt/libffi/lib -L/usr/local/opt/openssl@1.1/lib"
-# set -gx CPPFLAGS "-I/usr/local/opt/libffi/include -I/usr/local/opt/openssl@1.1/include"
-set -gx LDFLAGS "-L/usr/local/opt/libffi/lib -L/usr/local/opt/openssl@3/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/libffi/include -I/usr/local/opt/openssl@3/include"
+set -gx LDFLAGS "-L$(brew --prefix openssl@1.1)/lib -L$(brew --prefix libffi)/lib"
+set -gx CPPFLAGS "-I$(brew --prefix openssl@1.1)/include -I$(brew --prefix libffi)/include -I$(brew --prefix aspell)/include"
 
 # For pkg-config to find libffi
-set -gx PKG_CONFIG_PATH "/usr/local/opt/libffi/lib/pkgconfig"
+set -gx PKG_CONFIG_PATH "$(brew --prefix libffi)/lib/pkgconfig"
 
 # silence the console output of direnv
 set -gx DIRENV_LOG_FORMAT ""
+
+set -gx BAT_THEME "DarkNeon"
 
 # ssh kitty hack
 if test "$TERM" = "xterm-kitty"
@@ -33,8 +32,10 @@ if test "$TERM" = "xterm-kitty"
 end
 
 # fzf
-fzf_configure_bindings --directory=\cff --git_log=\сfl --git_status=\cfs --processes=\cfp --variables=\cfv
 set fzf_fd_opts --hidden --exclude=.git --no-ignore
+if type -q fisher
+  fzf_configure_bindings --directory=\cff --git_log=\сfl --git_status=\cfs --processes=\cfp --variables=\cfv
+end
 
 # nnn
 set -gx NNN_PLUG 'f:finder;o:fzopen;p:preview-tui;d:diffs;t:nmount;v:imgview'
