130af651c0ae826006d571b4df39a707e23581d0
Denis S <makafonov@gmail.com>
2024-06-28 07:55:57 +0500

    Regular update

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -8,10 +8,9 @@ alias config='/usr/local/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
 
 abbr -a -- gcd 'git checkout dev'
 
-# ls to exa
 alias l='ls'
-alias ls='exa -la --color=always --group-directories-first'
-alias lt='exa -aT --color=always --group-directories-first'  # tree
+alias ls='eza -la --color=always --group-directories-first'
+alias lt='eza -aT --color=always --group-directories-first'  # tree
 
 alias c2="awk '{ print \$2 }'"
 alias c3="awk '{ print \$3 }'"
